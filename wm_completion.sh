cea6ee0e184cdca5d17ff177856203e784ee15fc
Romain Mrad <romain.mrad@gmail.com>
2024-06-15 18:53:26 +0200

    Initial commit

---
+++ wm_completion.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+_wm_completions()
+{
+    local cur="${COMP_WORDS[COMP_CWORD]}"
+    local commands="start stop restart status config edit help"
+
+    COMPREPLY=( $(compgen -W "${commands}" -- ${cur}) )
+    return 0
+}
+
+complete -F _wm_completions wm
