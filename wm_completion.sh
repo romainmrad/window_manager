216669562b63f32de1cfe9b22025433b312fc477
Romain Mrad <romain.mrad@gmail.com>
2024-06-19 21:09:58 +0200

    Refactoring script location to export to path

--- wm_completion.sh
+++ wm_completion.sh
@@ -3,7 +3,15 @@
 _wm_completions()
 {
     local cur="${COMP_WORDS[COMP_CWORD]}"
-    local commands="start stop restart status config edit help"
+    local commands="
+	start 
+	stop 
+	restart 
+	status 
+	config 
+	edit 
+	help
+	"
 
     COMPREPLY=( $(compgen -W "${commands}" -- ${cur}) )
     return 0
